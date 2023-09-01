384063e605e5b4d35187aeca2aca03a9b19a02a7
NickElias01 <nicholas.elias95@gmail.com>
2023-09-01 07:55:11 -0600

    fruit sales exercise, added .csv

--- fruitsales.py
+++ fruitsales.py
@@ -5,6 +5,6 @@ import pandas as pd
 Fruit_Sales = pd.DataFrame({"Apples":[35,41], "Bananas":[21,34]}, index =  ["2017 Sales","2018 Sales"])
 
 #Create csv file
-Fruit_Sales.to_csv()
+Fruit_Sales.to_csv("fruit.csv")
 
 print(Fruit_Sales)
